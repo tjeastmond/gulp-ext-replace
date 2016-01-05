a9aaf1c6a5ba30f8480feeaf9548ef91c05d6d42
Danny <danny@skully.com>
2016-01-04 17:30:19 -0800

    FIX

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ var path = require('path');
 module.exports = function(ext, replaceExt, remove) {
     var extension_replace = function(file, encoding, callback) {
         replaceExt = replaceExt || false;
-        if (remove) {
-            file.path = file.path.replace(path.extname(file.path), '');
-        } else if (typeof ext === 'string' && ext.length > 0) {
+                        remove = remove || false;
+                        if (remove) {
+                            file.path = file.path.replace(path.extname(file.path), '');
+                        }
+        else if (typeof ext === 'string' && ext.length > 0) {
             ext = ext.indexOf('.') === 0 ? ext : '.' + ext;
             file.path = file.path.replace(replaceExt ? replaceExt : path.extname(file.path), ext);
         }
